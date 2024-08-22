6787fbf0e3a1762dcfd1fa0fdb8d5c918389bc02
Vadim <vadimkotkin1855@gmail.com>
2024-08-22 14:17:51 +0200

    modules

--- terraform-main/outputs.tf
+++ terraform-main/outputs.tf
@@ -17,10 +17,10 @@ output "instance_ids" {
 output "subnet_ids" {
   description = "The IDs of the VPC subnets used by the Yandex Compute instances."
   value = {
-    for subnet in yandex_vpc_subnet.private :
-    subnet.name => subnet.id...
+    for cidr_block, subnet_info in module.net.public_subnets :
+    subnet_info.name => subnet_info.subnet_id
   }
-} 
+}
 
 output "ydb_id" {
   description = "The ID of the Yandex Managed Service for YDB instance."
@@ -29,25 +29,25 @@ output "ydb_id" {
 
 output "service_account_id" {
   description = "The ID of the Yandex IAM service account."
-  value       = yandex_iam_service_account.bucket.id
+  value       = module.s3.storage_admin_service_account_id
 }
 
 output "bucket_name" {
   description = "The name of the Yandex Object Storage bucket."
-  value       = yandex_storage_bucket.this.bucket
-}
+  value       = module.s3.bucket_name
+} 
 
-output "access_key" {
-    description = "The public access key"
-    sensitive   = true
-    value       = yandex_iam_service_account_static_access_key.this.access_key
-}
+# output "access_key" {
+#     description = "The public access key"
+#     sensitive   = true
+#     value       = yandex_iam_service_account_static_access_key.this.access_key
+# }
 
-output "acess_secret_key" {
-    description = "The secret access key"
-    sensitive   = true
-    value       = yandex_iam_service_account_static_access_key.this.secret_key
-}
+# output "acess_secret_key" {
+#     description = "The secret access key"
+#     sensitive   = true
+#     value       = yandex_iam_service_account_static_access_key.this.secret_key
+# }
 
 output "instance_public_ip_addresses" {
   description = "The external IP addresses of the instances."
